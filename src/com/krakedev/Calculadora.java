90256b3cbcbc93724cd167ce7cb171261f93d088
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-09 18:43:54 -0500

    Funcion Dividir

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -3,10 +3,28 @@ package com.krakedev;
 public class Calculadora {
 	
 	public double multiplicar(double valor1, double valor2) {
-		return valor1 * valor2;
-		
+		double producto = valor1 + valor2;
+		return producto;
 	}
 	
+	public double dividir (double dividendo, double divisor) {
+		double cociente = dividendo / divisor;
+		return cociente;
+	}
+	
+	/*
+	 * public double promediar (double valor1, double valor2, double valor3) {
+		double promedioTotal = (valor1+valor2+valor3)/3;
+		return promedioTotal;
+	}
+	
+	public void mostrarResultado () {
+		System.out.println("Ahorita no joven, Sali al almuerzo. Regreso en 15 minutos");
+	}
+	 * 
+	 * 
+	 * 
+	 * */
 	
 
 }
