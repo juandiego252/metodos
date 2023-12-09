89cefdb3e951bff1953777bda94b778698c92bc3
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-09 18:58:30 -0500

    Metodos probados

--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -4,6 +4,13 @@ public class TestCalculadora {
 
 	public static void main(String[] args) {
 		
+		Calculadora operaciones = new Calculadora();
+		
+		System.out.println("Resultado Multiplicacion:" + operaciones.multiplicar(10, 5.2));
+		System.out.println("Resultado Dividir:" + operaciones.dividir(10, 2));
+		System.out.println("Resultado Promedio:" + operaciones.promediar(10,9, 8.25));
+		operaciones.mostrarResultado();
+		
 	}
 
 }
