45f937d7fb28eec1c2f649e023c1fc30361a6828
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-09 18:32:53 -0500

    Funcion Multiplicar Realizada

--- src/com/krakedev/Calculadora.java
+++ src/com/krakedev/Calculadora.java
@@ -4,6 +4,7 @@ public class Calculadora {
 	
 	public double multiplicar(double valor1, double valor2) {
 		return valor1 * valor2;
+		
 	}
 	
 	
--- src/com/krakedev/TestCalculadora.java
+++ src/com/krakedev/TestCalculadora.java
@@ -3,8 +3,7 @@ package com.krakedev;
 public class TestCalculadora {
 
 	public static void main(String[] args) {
-		Calculadora mul = new Calculadora();
-		System.out.println("El resultado es:" + mul.multiplicar(5, 10));
+		
 	}
 
 }
