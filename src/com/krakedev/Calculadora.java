1f7e8f0739e3d8c1250baaf1ae7d364ec6d51913
juandiegojd252 <juandiegojd252@gmail.com>
2023-12-09 18:30:40 -0500

    Configuraciones Iniciales

---
+++ src/com/krakedev/Calculadora.java
@@ -0,0 +1,11 @@
+package com.krakedev;
+
+public class Calculadora {
+	
+	public double multiplicar(double valor1, double valor2) {
+		return valor1 * valor2;
+	}
+	
+	
+
+}
---
+++ src/com/krakedev/TestCalculadora.java
@@ -0,0 +1,10 @@
+package com.krakedev;
+
+public class TestCalculadora {
+
+	public static void main(String[] args) {
+		Calculadora mul = new Calculadora();
+		System.out.println("El resultado es:" + mul.multiplicar(5, 10));
+	}
+
+}
